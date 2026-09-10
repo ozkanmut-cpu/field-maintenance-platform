ab9bc7a7e0a936b7d35c1168d1b199db14e83dee
ozkanmut-cpu <ozkanmut@gmail.com>
2026-09-10 10:47:28 +0300

    feat(prospects): register EFESIM confirmation service

--- apps/api/src/prospects/prospects.module.ts
+++ apps/api/src/prospects/prospects.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ProspectConfirmationService } from './prospect-confirmation.service';
 import { ProspectsController } from './prospects.controller';
 import { ProspectsService } from './prospects.service';
 
 @Module({
   controllers: [ProspectsController],
-  providers: [ProspectsService],
-  exports: [ProspectsService],
+  providers: [ProspectsService, ProspectConfirmationService],
+  exports: [ProspectsService, ProspectConfirmationService],
 })
 export class ProspectsModule {}
