843b34beb133765119b74b4dc45d719a138d9c36
ozkanmut-cpu <ozkanmut@gmail.com>
2026-09-10 11:01:29 +0300

    feat(prospects): register conversion service

--- apps/api/src/prospects/prospects.module.ts
+++ apps/api/src/prospects/prospects.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ProspectConfirmationService } from './prospect-confirmation.service';
+import { ProspectConversionService } from './prospect-conversion.service';
 import { ProspectsController } from './prospects.controller';
 import { ProspectsService } from './prospects.service';
 
 @Module({
   controllers: [ProspectsController],
-  providers: [ProspectsService, ProspectConfirmationService],
-  exports: [ProspectsService, ProspectConfirmationService],
+  providers: [ProspectsService, ProspectConfirmationService, ProspectConversionService],
+  exports: [ProspectsService, ProspectConfirmationService, ProspectConversionService],
 })
 export class ProspectsModule {}
