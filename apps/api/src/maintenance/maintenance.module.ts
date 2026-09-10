4e24173305b5cb893fec56b8031816baf07358fd
ozkanmut-cpu <ozkanmut@gmail.com>
2026-09-10 09:56:03 +0300

    feat(api): register anomaly engine

--- apps/api/src/maintenance/maintenance.module.ts
+++ apps/api/src/maintenance/maintenance.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MaintenanceAnomalyService } from './maintenance-anomaly.service';
 import { MaintenanceController } from './maintenance.controller';
 import { MaintenanceEngineService } from './maintenance-engine.service';
 import { MaintenanceService } from './maintenance.service';
 
 @Module({
   controllers: [MaintenanceController],
-  providers: [MaintenanceEngineService, MaintenanceService],
-  exports: [MaintenanceEngineService, MaintenanceService],
+  providers: [MaintenanceEngineService, MaintenanceAnomalyService, MaintenanceService],
+  exports: [MaintenanceEngineService, MaintenanceAnomalyService, MaintenanceService],
 })
 export class MaintenanceModule {}
