2c69824c07d6af6aaa2438b19b2a28a1535e17c9
ozkanmut-cpu <ozkanmut@gmail.com>
2026-09-10 11:08:59 +0300

    feat(maintenance): register obligation history service

--- apps/api/src/maintenance/maintenance.module.ts
+++ apps/api/src/maintenance/maintenance.module.ts
@@ -4,19 +4,22 @@ import { GooglePlaceMatchService } from './google-place-match.service';
 import { MaintenanceController } from './maintenance.controller';
 import { MaintenanceAnomalyService } from './maintenance-anomaly.service';
 import { MaintenanceEngineService } from './maintenance-engine.service';
+import { MaintenanceObligationController } from './maintenance-obligation.controller';
+import { MaintenanceObligationService } from './maintenance-obligation.service';
 import { MaintenanceService } from './maintenance.service';
 import { NonMaintenanceVisitService } from './non-maintenance-visit.service';
 import { PointLocationLearningService } from './point-location-learning.service';
 
 @Module({
   imports: [AssignmentsModule],
-  controllers: [MaintenanceController],
+  controllers: [MaintenanceController, MaintenanceObligationController],
   providers: [
     MaintenanceEngineService,
     MaintenanceAnomalyService,
     PointLocationLearningService,
     GooglePlaceMatchService,
     NonMaintenanceVisitService,
+    MaintenanceObligationService,
     MaintenanceService,
   ],
   exports: [
@@ -25,6 +28,7 @@ import { PointLocationLearningService } from './point-location-learning.service'
     PointLocationLearningService,
     GooglePlaceMatchService,
     NonMaintenanceVisitService,
+    MaintenanceObligationService,
     MaintenanceService,
   ],
 })
