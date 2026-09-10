92bce01b25b64fc06f2b5449baa1d6f15cf99472
ozkanmut-cpu <ozkanmut@gmail.com>
2026-09-10 09:36:07 +0300

    feat(api): add point DTO

---
+++ apps/api/src/points/dto/create-point.dto.ts
@@ -0,0 +1,39 @@
+import { MaintenanceType, PointStatus } from '@prisma/client';
+import { IsEnum, IsInt, IsOptional, IsString, IsUUID, Max, MaxLength, Min, MinLength, ValidateIf } from 'class-validator';
+
+export class CreatePointDto {
+  @IsString()
+  @MinLength(1)
+  @MaxLength(64)
+  code!: string;
+
+  @IsString()
+  @MinLength(2)
+  @MaxLength(200)
+  name!: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(500)
+  address?: string;
+
+  @IsUUID()
+  regionId!: string;
+
+  @IsOptional()
+  @IsEnum(PointStatus)
+  status?: PointStatus;
+
+  @IsEnum(MaintenanceType)
+  maintenanceType!: MaintenanceType;
+
+  @ValidateIf((o) => o.maintenanceType === MaintenanceType.STANDARD)
+  @IsInt()
+  @Min(1)
+  @Max(2)
+  maintenanceWeek?: number;
+
+  @ValidateIf((o) => o.maintenanceType === MaintenanceType.SMARTCLEAN)
+  @IsString()
+  smartcleanReferenceAt?: string;
+}
