3e40c2d65fc180fa4a475f92c5aa551bba188ab1
ozkanmut-cpu <ozkanmut@gmail.com>
2026-09-12 09:25:19 +0300

    feat(points): wire automatic address discovery

--- apps/api/src/points/points.module.ts
+++ apps/api/src/points/points.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AssignmentsModule } from '../assignments/assignments.module';
+import { PointAddressDiscoveryService } from './point-address-discovery.service';
 import { PointsController } from './points.controller';
 import { PointsService } from './points.service';
 
 @Module({
   imports: [AssignmentsModule],
   controllers: [PointsController],
-  providers: [PointsService],
-  exports: [PointsService],
+  providers: [PointsService, PointAddressDiscoveryService],
+  exports: [PointsService, PointAddressDiscoveryService],
 })
 export class PointsModule {}
